14eb57d5a7cdce365eeb7230e70b78506a1c5a4e
Mark Lubega <lubegamark@gmail.com>
2016-10-12 11:13:11 +0300

    Add RecyclerView and Picasso

--- app/src/main/java/com/lubegamark/booksug/MainActivityFragment.java
+++ app/src/main/java/com/lubegamark/booksug/MainActivityFragment.java
@@ -2,14 +2,24 @@ package com.lubegamark.booksug;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private View rootView;
 
     public MainActivityFragment() {
     }
@@ -17,6 +27,26 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        String[] myDataset = new String[]{"Java" , "Scala" , "Groovy"};
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.book_list);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/lubegamark/booksug/MyAdapter.java
@@ -0,0 +1,72 @@
+package com.lubegamark.booksug;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTitleView;
+        public TextView mAuthorView;
+        public ImageView mThumbnailView;
+
+        public ViewHolder(View v) {
+            super(v);
+            mTitleView =  (TextView) v.findViewById(R.id.book_list_title);
+            mAuthorView = (TextView)  v.findViewById(R.id.book_list_author);
+            mThumbnailView = (ImageView)  v.findViewById(R.id.book_list_image);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        return new ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        Log.d("Item", mDataset[position]);
+        holder.mTitleView.setText(mDataset[position]);
+        holder.mAuthorView.setText(mDataset[position]);
+        Context context = holder.mThumbnailView.getContext();
+        Picasso.with(context)
+                .load('/')
+                .placeholder(R.drawable.ic_book)
+                .error(R.drawable.ic_book)
+                .into(holder.mThumbnailView);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
